038bf38c55f0f242d3092e9d475c8c458932dba6
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-09-15 01:11:26 +0530

    test server

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     path: '',
   },
   env: {
-    API: AWS,
+    API: TEST_SERVER,
     SPLTOKENACCOUNTSPAREKEY: process.env.SPLTOKENACCOUNTSPAREKEY,
     MEAN_SPL_TOKEN: 'MEANeD3XDdUmNMsRGjASkSWdC8prLYsoRJ61pPeHctD',
     SOLRAZR_SPL_TOKEN: '7j7H7sgsnNDeCngAPjpaCN4aaaru4HS7NAFYSEUyzJ3k',
