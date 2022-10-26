c26a12023bdfe07b67f2121e4456c81bac00e7c0
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-10-26 19:29:34 +0530

    image fix

--- components/DAOCard/index.jsx
+++ components/DAOCard/index.jsx
@@ -23,7 +23,7 @@ export default function DAOCard({ data }) {
             <div className={styles.card}>
                 {(data.dao_name.length > 20) && < ReactTooltip backgroundColor={"#747c90"} />}
                 <div className={styles.cover} >
-                    <img src={data.dao_cover} alt="" onError={({ currentTarget }) => {
+                    <img src={`${data.dao_cover}/600x200`} alt="" onError={({ currentTarget }) => {
                         currentTarget.src = "https://upload.wikimedia.org/wikipedia/commons/e/e3/Pink_tints_and_shades.svg";
                     }} />
                 </div>
@@ -55,7 +55,7 @@ export default function DAOCard({ data }) {
                                 (data.twitter_link) && openNewTab(data.twitter_link)
                                 e.stopPropagation()
                             }} />
-                            <p  onClick={(e) => {
+                            <p onClick={(e) => {
                                 (data.twitter_link) && openNewTab(data.twitter_link)
                                 e.stopPropagation()
                             }} >
