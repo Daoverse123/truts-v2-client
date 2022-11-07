bc6344b96ac5c1cf7892f3806ae89bab3da9b42f
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-11-07 16:43:43 +0530

    switch api

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     path: '',
   },
   env: {
-    API: LOCALHOST,
+    API: AWS,
     SPLTOKENACCOUNTSPAREKEY: process.env.SPLTOKENACCOUNTSPAREKEY,
 
     MEAN_SPL_TOKEN: 'MEANeD3XDdUmNMsRGjASkSWdC8prLYsoRJ61pPeHctD',
