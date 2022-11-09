b95e3d7edbdd6396082617ec0ac2b3ab1ffdd997
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-11-09 18:14:10 +0530

    branch build

--- pages/discover/index.jsx
+++ pages/discover/index.jsx
@@ -477,7 +477,7 @@ const getDynamicLoad = async (daoList_ssr, setdaoList_ssr, paginationConfig, set
         setter(catCount)
     }
 
-    //staging build
+    //staging build new
 
     generateCatCount(CATEGORY_LIST, 'dao_category', setcatCount)
     generateChainCount(CHAIN_LIST, 'chain', setchainCount)
