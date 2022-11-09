8f25160e2e77d48064e9fc33ec60cba3dd48b16f
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-11-09 18:03:03 +0530

    update

--- pages/discover/index.jsx
+++ pages/discover/index.jsx
@@ -477,8 +477,8 @@ const getDynamicLoad = async (daoList_ssr, setdaoList_ssr, paginationConfig, set
         setter(catCount)
     }
 
-    //staging
-    
+    //staging c
+
     generateCatCount(CATEGORY_LIST, 'dao_category', setcatCount)
     generateChainCount(CHAIN_LIST, 'chain', setchainCount)
 
