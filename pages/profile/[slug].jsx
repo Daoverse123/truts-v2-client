cdfe9798a6a10a24c700887d3bed02ef20baf4f8
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2023-02-20 23:16:10 +0530

    update calc

--- pages/profile/[slug].jsx
+++ pages/profile/[slug].jsx
@@ -379,7 +379,7 @@ function Profile({ slug }) {
                   </h3>
 
                   <p>
-                    {uuserData.xp.totalTrutsXP}/
+                    {userData.xp.totalTrutsXP}/
                     {parseInt(userData.xp.level.xpForNextLevel) +
                       parseInt(userData.xp.totalTrutsXP)}
                   </p>
