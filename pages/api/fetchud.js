57e88c736513536524ca074f4c4b6f0153395d71
“John <johnswaroop28@gmail.com>
2023-06-01 18:02:30 +0530

    ud error update

--- pages/api/fetchud.js
+++ pages/api/fetchud.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 
 const fetchDomain = async (address) => {
+
   let res = await axios.get(`https://resolve.unstoppabledomains.com/reverse/${address}`, {
     headers: {
       Authorization: `Bearer ${process.env.UD_API}`
@@ -12,16 +13,23 @@ const fetchDomain = async (address) => {
 
 
 export default async function handler(req, res) {
-  let address = req.query.address
-  let domain = await fetchDomain(`${address}`.toLowerCase());
-  console.log(domain.data.meta)
-  if (domain.status == 200) {
-    res.status(200).send(domain.data.meta);
+  try {
+    let address = req.query.address
+    let domain = await fetchDomain(`${address}`.toLowerCase());
+    console.log(domain.data.meta)
+    if (domain.status == 200) {
+      res.status(202).send(domain.data.meta);
+    }
+    else {
+      res.status(204).send({
+        error: "not found"
+      });
+    }
   }
-  else {
-    res.status(404).send({
-      error: "not found"
+  catch (err) {
+    console.log(err)
+    res.status(204).send({
+      error: "internal server error"
     });
   }
-
 } 
